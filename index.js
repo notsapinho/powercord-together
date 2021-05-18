aae897635503b1a7188e199be42ab758790dfe94
Koray <public@d3xx3r.de>
2021-05-18 20:10:03 +0200

    Update way to find the experiment
    This filters the experiments to find the one with a certain label so no one has to do it manually.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { inject, uninject } = require("powercord/injector");
 const { Plugin } = require("powercord/entities");
 
 const { YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID } = getModule(["GENERIC_EVENT_EMBEDDED_APPS"], false);
-const useExperiment = getAllModules(["useExperiment"], false)[36];
+const useExperiment = getAllModules(["useExperiment"], false).filter(obj => obj.definition.label == "Activities Experiment")[0];
 const getGuild = getModule(["getGuild"], false);
 
 const ids = [YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, "832012586023256104"];
