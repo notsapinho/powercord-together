e373e38465540ec5a6a14ab3678bd2b35a8b8163
notsapinho <52896767+notsapinho@users.noreply.github.com>
2021-08-14 23:41:10 -0300

    Fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { inject, uninject } = require("powercord/injector");
 const { Plugin } = require("powercord/entities");
 
 const { YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID } = getModule(["GENERIC_EVENT_EMBEDDED_APPS"], false);
-const useExperiment = getAllModules(["useExperiment"], false).filter(obj => obj.definition.label == "Activities Experiment")[0];
+const useExperiment = getAllModules(["useExperiment"], false).filter(obj => obj?.definition?.label === "Activities Experiment")[0];
 const getGuild = getModule(["getGuild"], false);
 
 const ids = [YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, "832012586023256104"];
@@ -17,7 +17,7 @@ module.exports = class PowercordTogether extends Plugin {
         });
 
         inject("powercord-together-rocket", useExperiment, "useExperiment", (args, res) => {
-            if (!args[0].guildId) return res;
+            if (args[0].guildId === "") return res;
 
             if (!res[0]?.enabledApplicationIds?.length) {
                 res[0].enabledApplicationIds = ids;
