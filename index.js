8ab2df0f2506d68ae5fd2fa00c1251602a940145
notsapinho <52896767+notsapinho@users.noreply.github.com>
2021-09-20 16:19:09 -0300

    fix new update

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
-const { getModule, getAllModules } = require("powercord/webpack");
+const { getModule } = require("powercord/webpack");
 const { inject, uninject } = require("powercord/injector");
 
 const { Plugin } = require("powercord/entities");
 
-const { YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, CHESS_IN_THE_PARK_APPLICATION_ID } = getModule(["YOUTUBE_APPLICATION_ID"], false);
-const useExperiment = getModule(obj => obj?.definition?.label === "Activities Experiment", false)
-const getGuild = getModule(["getGuild"], false);
-
-const ids = [YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, CHESS_IN_THE_PARK_APPLICATION_ID];
+const {
+	YOUTUBE_APPLICATION_ID,
+	POKER_NIGHT_APPLICATION_ID,
+	FISHINGTON_APPLICATION_ID,
+	END_GAME_APPLICATION_ID,
+	CHESS_IN_THE_PARK_APPLICATION_ID
+} = getModule(["YOUTUBE_APPLICATION_ID"], false);
 
 module.exports = class PowercordTogether extends Plugin {
-    async startPlugin() {
-        inject("powercord-together-region", getGuild, "getGuild", (args, res) => {
-            if (res) res.region = "us-west";
-            return res;
-        });
+	async startPlugin() {
+		inject("powercord-together-region", getModule(["getGuild"], false), "getGuild", (args, res) => {
+			if (res) res.region = "us-west";
+			return res;
+		});
 
-        inject("powercord-together-rocket", useExperiment, "useExperiment", (args, res) => {
-            if (args[0].guildId === "" || !args[0].guildId) return res;
+		inject("powercord-together-ids", getModule(["getEnabledAppIds"], false), "getEnabledAppIds", (args, res) => {
+			res = [
+				YOUTUBE_APPLICATION_ID,
+				POKER_NIGHT_APPLICATION_ID,
+				FISHINGTON_APPLICATION_ID,
+				END_GAME_APPLICATION_ID,
+				CHESS_IN_THE_PARK_APPLICATION_ID
+			];
+			return res;
+		});
 
-            if (!res[0]?.enabledApplicationIds?.length) {
-				res[0] = {
-					enabledApplicationIds: ids,
-					rtcPanelIconsOnly: true,
-					showDiscordGameTooltips: false,
-					enableActivities: true,
-					useNewInviteButton: true
+		inject(
+			"powercord-together-rocket",
+			getModule((obj) => obj?.definition?.label === "Activities Experiment", false),
+			"useExperiment",
+			(args, res) => {
+				if (!args[0].guildId) return res;
+
+				if (!res[0]?.enabledApplicationIds?.length) {
+					res[0] = {
+						rtcPanelIconsOnly: true,
+						showDiscordGameTooltips: false,
+						enableActivities: true,
+						useNewInviteButton: true
+					};
 				}
-            }
 
-            return res;
-        });
-    }
+				return res;
+			}
+		);
+	}
 
-    pluginWillUnload() {
-        uninject("powercord-together-region");
-        uninject("powercord-together-rocket");
-    }
+	pluginWillUnload() {
+		uninject("powercord-together-region");
+		uninject("powercord-together-rocket");
+		uninject("powercord-together-ids");
+	}
 };
