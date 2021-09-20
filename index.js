ce8a914555974babf615060bebc9be40a81ffcff
honeytequila <rafaelalibibi@gmail.com>
2021-09-20 09:05:57 -0300

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { inject, uninject } = require("powercord/injector");
 
 const { Plugin } = require("powercord/entities");
 
-const { YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, CHESS_IN_THE_PARK_APPLICATION_ID } = getModule(["GENERIC_EVENT_EMBEDDED_APPS"], false);
-const useExperiment = getAllModules(["useExperiment"], false).filter(obj => obj?.definition?.label === "Activities Experiment")[0];
+const { YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, CHESS_IN_THE_PARK_APPLICATION_ID } = getModule(["YOUTUBE_APPLICATION_ID"], false);
+const useExperiment = getModule(obj => obj?.definition?.label === "Activities Experiment", false)
 const getGuild = getModule(["getGuild"], false);
 
 const ids = [YOUTUBE_APPLICATION_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID, CHESS_IN_THE_PARK_APPLICATION_ID];
@@ -20,10 +20,13 @@ module.exports = class PowercordTogether extends Plugin {
             if (args[0].guildId === "" || !args[0].guildId) return res;
 
             if (!res[0]?.enabledApplicationIds?.length) {
-                res[0].enabledApplicationIds = ids;
-                res[0].rtcPanelIconsOnly = true;
-                res[0].showDiscordGameTooltips = true;
-                res[0].useNewInviteButton = true;
+				res[0] = {
+					enabledApplicationIds: ids,
+					rtcPanelIconsOnly: true,
+					showDiscordGameTooltips: false,
+					enableActivities: true,
+					useNewInviteButton: true
+				}
             }
 
             return res;
