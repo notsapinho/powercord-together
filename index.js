194a537fe7d9c5c1b017b648fc6d0a142a220bc5
honeytequila <rafaelalibibi@gmail.com>
2021-10-13 20:21:43 -0300

    fix

--- index.js
+++ index.js
@@ -3,55 +3,35 @@ const { inject, uninject } = require("powercord/injector");
 
 const { Plugin } = require("powercord/entities");
 
-const {
-	YOUTUBE_APPLICATION_ID,
-	POKER_NIGHT_APPLICATION_ID,
-	FISHINGTON_APPLICATION_ID,
-	END_GAME_APPLICATION_ID,
-	CHESS_IN_THE_PARK_APPLICATION_ID
-} = getModule(["YOUTUBE_APPLICATION_ID"], false);
+const { WATCH_YOUTUBE_PROD_APP_ID, POKER_NIGHT_APPLICATION_ID, FISHINGTON_APPLICATION_ID, END_GAME_APPLICATION_ID } = getModule(
+	["WATCH_YOUTUBE_PROD_APP_ID"],
+	false
+);
 
 module.exports = class PowercordTogether extends Plugin {
 	async startPlugin() {
-		inject("powercord-together-region", getModule(["getGuild"], false), "getGuild", (args, res) => {
-			if (res) res.region = "us-west";
-			return res;
-		});
+		const Activities = getModule(["getEnabledAppIds"], false);
 
-		inject("powercord-together-ids", getModule(["getEnabledAppIds"], false), "getEnabledAppIds", (args, res) => {
-			res = [
-				YOUTUBE_APPLICATION_ID,
+		inject("powercord-together-ids", Activities.__proto__, "getEnabledAppIds", () => {
+			return [
+				WATCH_YOUTUBE_PROD_APP_ID,
 				POKER_NIGHT_APPLICATION_ID,
 				FISHINGTON_APPLICATION_ID,
 				END_GAME_APPLICATION_ID,
-				CHESS_IN_THE_PARK_APPLICATION_ID
+				// some games that i couldn't find except by manually grabbing from discordgamelab server
+				"832012774040141894", // chess
+				"878067389634314250", // doodle
+				"879863976006127627", // word snacks
+				"879863686565621790" // letter tile
 			];
-			return res;
 		});
 
-		inject(
-			"powercord-together-rocket",
-			getModule((obj) => obj?.definition?.label === "Activities Experiment", false),
-			"useExperiment",
-			(args, res) => {
-				if (!args[0].guildId) return res;
-
-				if (!res[0]?.enabledApplicationIds?.length) {
-					res[0] = {
-						rtcPanelIconsOnly: true,
-						showDiscordGameTooltips: false,
-						enableActivities: true,
-						useNewInviteButton: true
-					};
-				}
-
-				return res;
-			}
-		);
+		inject("powercord-together-rocket", Activities.__proto__, "isActivitiesEnabled", () => {
+			return true;
+		});
 	}
 
 	pluginWillUnload() {
-		uninject("powercord-together-region");
 		uninject("powercord-together-rocket");
 		uninject("powercord-together-ids");
 	}
